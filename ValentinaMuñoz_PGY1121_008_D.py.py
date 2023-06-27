3b7b1daae89ff2ed14b6b19619ab5b8a9b4c8988
Valhentina <137181466+Valhentina@users.noreply.github.com>
2023-06-27 10:43:05 -0400

    Add files via upload

---
+++ ValentinaMuñoz_PGY1121_008_D.py.py
@@ -0,0 +1,40 @@
+
+def validar_opcion()
+def validar_busqueda()
+def validar_pago()
+
+
+while True:
+        try:
+                opcion = print("""Hola! Bienvenido a LPS Hotel porfavor eliga una opción:
+                1.- Ingreso
+                2.- Busqueda de perrito
+                3.- Pago
+                4.- Salida""")    
+
+        if print("Seleccionado")           
+
+        else print("ERROR! Digito no disponible")
+                break
+                
+                while True:
+                        opcion 1 = print ("Ingrese su nombre, rut, nombre de mascota:"):
+                                 print (input("Ingrese digito numerico, cuantos días se quedara su mascota (MÁX 60 días)"))
+                                if print ("Vamos bien!"):
+                                
+                                else 
+                                
+                                
+
+
+                
+
+
+
+    
+
+                      
+
+        
+
+
